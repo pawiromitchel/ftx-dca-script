a2803fc8a85ceeb0bd86fbc8f420cb9f0c3e6f0f
Mitchel Pawirodinomo <pawiromitchel@gmail.com>
2021-09-10 15:52:26 -0300

    refactor code

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ require("dotenv").config();
 const cron = require('node-cron');
 const FTX = require('./ftx.class');
 
+const creds = {
+    'key': process.env.KEY,
+    'secret': process.env.SECRET,
+    'subaccount': process.env.SUBACCOUNT
+}
+
+const FTX_INSTANCE = new FTX(creds);
+
 // convert 1 cent to btc
 const FROM = 'USD';
 const TO = 'BTC';
@@ -11,34 +19,27 @@ const ORDERSIZE = 0.01;
 // https://crontab.guru/
 const SCHEDULE = '0 0 * * 2';
 
-const creds = {
-    'key': process.env.KEY,
-    'secret': process.env.SECRET,
-    'subaccount': process.env.SUBACCOUNT
-}
-
 async function main(_from, _to, _size) {
     // if all the arguments are met
     if (arguments.length) {
-        const ftx = new FTX(creds);
         console.log('[i] Script ran at: ', new Date().toString())
         // get the exchange rate of the swap you want to do
-        const getPrice = await ftx.getPrice(`${_to}/${_from}`);
+        const getPrice = await FTX_INSTANCE.getPrice(`${_to}/${_from}`);
         if (getPrice) {
             const { price, quoteCurrency } = getPrice.result;
             console.log(`[i] Exchange rate: ${price} ${quoteCurrency}`);
         }
         // set up the conversion
-        const convert = await ftx.convert(_from, _to, _size);
+        const convert = await FTX_INSTANCE.convert(_from, _to, _size);
         if (convert) {
             const { quoteId } = convert.result;
             if (quoteId) {
                 console.log(`[i] QuoteId: ${quoteId}`);
                 // get the information about the conversion you are about to do
-                const info = await ftx.getQuoteInfo(quoteId);
+                const info = await FTX_INSTANCE.getQuoteInfo(quoteId);
                 const { fromCoin, toCoin, proceeds } = info.result;
                 // accept the conversion
-                const acceptOrder = await ftx.acceptQuote(quoteId);
+                const acceptOrder = await FTX_INSTANCE.acceptQuote(quoteId);
                 const { success } = acceptOrder;
                 if (success) {
                     console.log(`[i] Converted ${_size} ${fromCoin} to ${proceeds.toFixed(8)} ${toCoin}`);
@@ -59,3 +60,6 @@ async function main(_from, _to, _size) {
 cron.schedule(SCHEDULE, () => {
     main(FROM, TO, ORDERSIZE);
 });
+
+// for testing purposes
+// main(FROM, TO, ORDERSIZE);
